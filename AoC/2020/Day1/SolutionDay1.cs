55b14c4224260d697a8f8833598b616854f793df
ChrilleSigerhed <57347249+ChrilleSigerhed@users.noreply.github.com>
2020-12-02 11:36:44 +0100

    Add project files.

---
+++ AoC/2020/Day1/SolutionDay1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+
+public class SolutionDay1
+{
+    public string[] Input { get; set; } = File.ReadAllLines(@"C:\Users\ChrillE\source\repos\AoC\AoC\2020\Day1\Input.txt");
+
+    public int[] Solution1()
+    {
+        int[] result = new int[2];
+        for (int i = 0; i < Input.Length; i++)
+        {
+            for (int j = 0; j < Input.Length; j++)
+            {
+                if (int.Parse(Input[i]) + int.Parse(Input[j]) == 2020)
+                {
+                    result[0] = int.Parse(Input[i]);
+                    result[1] = int.Parse(Input[j]);
+                }
+            }
+        }
+        return result;
+    }
+    public int[] Solution2()
+    {
+        int[] result = new int[3];
+        for (int i = 0; i < Input.Length; i++)
+        {
+            for (int j = 0; j < Input.Length; j++)
+            {
+                for (int k = 0; k < Input.Length; k++)
+                {
+                    if (int.Parse(Input[i]) + int.Parse(Input[j]) + int.Parse(Input[k]) == 2020)
+                    {
+                        result[0] = int.Parse(Input[i]);
+                        result[1] = int.Parse(Input[j]);
+                        result[2] = int.Parse(Input[k]);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
+
+
---
+++ AoC/2020/Day2/SolutionDay2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+public class SolutionDay2
+{
+    public string[] Input { get; set; } = File.ReadAllLines(@"C:\Users\ChrillE\source\repos\AoC\AoC\2020\Day2\Input.txt");
+
+    public int Solution1()
+    {
+        int maxValue;
+        int minValue;
+        int counter;
+        int correctPassword = 0;
+        for (int i = 0; i < Input.Length; i++)
+        {
+            counter = 0;
+            string[] sortedInput = Input[i].Split(" ");
+            string[] range = sortedInput[0].Split("-");
+            minValue = int.Parse(range[0]); maxValue = int.Parse(range[1]);
+            for (int j  = 0; j < sortedInput[2].Length; j++)
+            {
+                if (sortedInput[2][j] == sortedInput[1][0])
+                {
+                    counter++;
+                }
+            }
+            if (counter >= minValue && counter <= maxValue)
+            {
+                correctPassword++;
+            }
+        }
+        return correctPassword;
+    }
+    public int Solution2()
+    {
+        int maxValue;
+        int minValue;
+        int counter;
+        int correctPassword = 0;
+        for (int i = 0; i < Input.Length; i++)
+        {
+            counter = 0;
+            string[] sortedInput = Input[i].Split(" ");
+            string[] range = sortedInput[0].Split("-");
+            minValue = int.Parse(range[0]); maxValue = int.Parse(range[1]);
+            if (sortedInput[2][minValue-1] == sortedInput[1][0])
+            {
+                counter++;
+            }
+            if (sortedInput[2][maxValue-1] == sortedInput[1][0])
+            {
+                counter++;
+            }
+            if(counter == 1)
+            {
+                correctPassword++;
+            }
+        }
+        return correctPassword;
+    }
+}
+
