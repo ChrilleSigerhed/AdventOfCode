f67a64ff0c84eabcc9bbb1b903f6f17b1f15cfce
ChrilleSigerhed <57347249+ChrilleSigerhed@users.noreply.github.com>
2020-12-03 16:05:17 +0100

    Divided Day3 Solution2 into smaller methods

--- AoC/2020/Day3/SolutionDay3.cs
+++ AoC/2020/Day3/SolutionDay3.cs
@@ -7,7 +7,8 @@ using System.Text;
 public class SolutionDay3
 {
     public string[] Input { get; set; } = File.ReadAllLines(@"C:\Users\ChrillE\source\repos\AoC\AoC\2020\Day3\Input.txt");
-   
+    public char Trees { get; set; } = '#';
+
     public int Solution1()
     {
         char tree = '#';
@@ -29,58 +30,58 @@ public class SolutionDay3
     }
     public int Solution2()
     {
-        char tree = '#';
-        int slope1 = 0;
-        int slope2 = 2;
-        int slope3 = 4;
-        int slope4 = 6;
-        int slope5 = 0;
-        int treeCounter1 = 0; 
-        int treeCounter2 = 0; 
-        int treeCounter3 = 0; 
-        int treeCounter4 = 0; 
-        int treeCounter5 = 0;
+        int slope = 0;
+        int result = 0;
+        for (int i = 0; i < 8; i+=2)
+        {
+            if(result != 0)
+            {
+                result *= SlopesWithoutJump(i);
+            }
+            else
+            {
+                result = SlopesWithoutJump(i);
+            }
+        }
+        result *= SlopesWithJumps(slope);
+        
+        return result;
+    }
+    private int SlopesWithoutJump(int slope)
+    {
+        int slopecounter = slope;
+        int treeCounter = 0;
         for (int i = 1; i < Input.Length; i++)
         {
-            while (Input[i].Length <= slope4)
+            while (Input[i].Length <= slopecounter+2)
             {
                 Input[i] += Input[i];
             }
-            if (Input[i][slope1 + 1] == tree)
+            if (Input[i][slopecounter + 1] == Trees)
             {
-                treeCounter1++;
+                treeCounter++;
             }
-            if (Input[i][slope2 + 1] == tree)
-            {
-                treeCounter2++;
-            }
-            if (Input[i][slope3 + 1] == tree)
-            {
-                treeCounter3++;
-            }
-            if (Input[i][slope4 + 1] == tree)
-            {
-                treeCounter4++;
-            }
-            slope1 += 1;
-            slope2 += 3;
-            slope3 += 5;
-            slope4 += 7;
+            slopecounter += slope + 1;
         }
+        return treeCounter;
+    }
+    private int SlopesWithJumps(int slope)
+    {
+        int slopecounter = slope;
+        int treeCounter = 0;
         for (int i = 2; i < Input.Length; i+=2)
         {
-            while (Input[i].Length <= slope5)
+            while (Input[i].Length <= slopecounter + 2)
             {
                 Input[i] += Input[i];
             }
-            if (Input[i][slope5 + 1] == tree)
+            if (Input[i][slopecounter + 1] == Trees)
             {
-                treeCounter5++;
+                treeCounter++;
             }
-            slope5++;
+            slopecounter += slope + 1;
         }
-        int result = treeCounter1 * treeCounter2 * treeCounter3 * treeCounter4 * treeCounter5;
-        return result;
+        return treeCounter;
     }
 }
 
